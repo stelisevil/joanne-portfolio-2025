16f63e90e27cb32fb64dda59ddede6b5f3b0ee62
Stelios Tsinontas <41406137+stelisevil@users.noreply.github.com>
2025-01-21 22:08:20 +0000

    updated home page copy

--- src/MainContent/HomePage/index.jsx
+++ src/MainContent/HomePage/index.jsx
@@ -1,25 +1,25 @@
 const HomePage = ({ recentWork, setContent }) => (
   <>
-    <div className="text-left w-full lg:w-2/3 mb-12">
+    <div className="w-full mb-12 text-left lg:w-2/3">
       <div className="mb-2 text-4xl font-bold">Hi, I'm Joanne.</div>
-      <div className="mb-7 text-4xl font-bold">
-        I'm a digital designer who loves to create new ideas.
+      <div className="text-4xl font-bold mb-7">
+        I'm a UX designer who loves to create new ideas.
       </div>
       <div className="my-2">
         With over ten years' professional experience, my skills span pretty much
-        every element of digital design, from UX and UI, to video editing for
-        social media, to HTML/CSS, to graphic design and art direction. Not
+        every element of digital design, from specialising in UX and UI, to
+        video editing, to HTML/CSS, to graphic design and art direction. Not
         knowing how to do something isn't a barrier - I'm a fast learner, and my
         knowledge has constantly evolved throughout my career.
       </div>
     </div>
     <div className="mb-12">
-      <p className="font-bold mb-4 text-xl">Recent work...</p>
-      <div className="flex md:flex-row flex-col w-full md:-m-2">
+      <p className="mb-4 text-xl font-bold">Recent work...</p>
+      <div className="flex flex-col w-full md:flex-row md:-m-2">
         <img
           src={recentWork.leftImage.url}
           alt="recent work images"
-          className="h-full md:w-1/3 w-full md:mx-1 md:p-1 cursor-pointer"
+          className="w-full h-full cursor-pointer md:w-1/3 md:mx-1 md:p-1"
           onClick={() => {
             setContent("ux-and-ui");
           }}
@@ -27,7 +27,7 @@ const HomePage = ({ recentWork, setContent }) => (
         <img
           src={recentWork.centralImage.url}
           alt="recent work images"
-          className="h-full md:w-1/3 w-full py-2 md:p-1 cursor-pointer"
+          className="w-full h-full py-2 cursor-pointer md:w-1/3 md:p-1"
           onClick={() => {
             setContent("graphic-design");
           }}
@@ -35,7 +35,7 @@ const HomePage = ({ recentWork, setContent }) => (
         <img
           src={recentWork.rightImage.url}
           alt="recent work images"
-          className="h-full md:w-1/3 w-full md:mx-1 md:p-1 cursor-pointer"
+          className="w-full h-full cursor-pointer md:w-1/3 md:mx-1 md:p-1"
           onClick={() => {
             setContent("ux-and-ui");
           }}
